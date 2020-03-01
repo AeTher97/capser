af46523ab8e19c46db5053b2be066a1c2b649fa2
micha <michal.wozniak@viviclabs.com>
2020-03-01 01:15:25 +0100

    Fixed days number being subzero.

---
+++ src/main/resources/static/precache-manifest.cd2c8b8a61e8537199cbec66ff0618b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0f9a80aa49e065ea2810e096991514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d16854f835f2b02666",
+    "url": "/static/css/main.434f66bc.chunk.css"
+  },
+  {
+    "revision": "99116e516948caccbde0",
+    "url": "/static/js/2.8604ee30.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/static/js/2.8604ee30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d16854f835f2b02666",
+    "url": "/static/js/main.ef9e0d9e.chunk.js"
+  },
+  {
+    "revision": "ca788f8bb8f2b213bde1",
+    "url": "/static/js/runtime-main.bfe3705b.js"
+  },
+  {
+    "revision": "7ba4f092bf6a8ec596cfb3fd633aedbf",
+    "url": "/static/media/BankGothicRegular.7ba4f092.ttf"
+  }
+]);
